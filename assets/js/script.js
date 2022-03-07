abc27666b32964ddf45eb21bf6e8b23b64de7cec
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-03-06 19:05:33 -0600

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 var getButtonEl = document.querySelector("#start-button")
+var getTimerEl = document.querySelector("#count-down")
+let i = 30 
 
 function startGame() {
     console.log("game has started")
 }
 
-var myfunc = setInterval(function() {
-    //code goes here
+function endGame() {
+    console.log("game has ended")
+    clearInterval(countDown)
+}
+
+var countDown = setInterval(function() {
+    if (i > 0) {
+    i--;
+    getTimerEl.textContent="Timer:" + i;
+    } else { 
+        endGame();
+    } 
 }, 1000
+    
 )
 
 getButtonEl.addEventListener("click", startGame)
